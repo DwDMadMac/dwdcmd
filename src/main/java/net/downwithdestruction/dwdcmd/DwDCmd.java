d6176a6c604370240ddf812d368a72e59c197389
MadMac MacAllister <madmac@downwithdestruction.net>
2015-10-19 22:18:06 -0400

    Description Exception..

--- src/main/java/net/downwithdestruction/dwdcmd/DwDCmd.java
+++ src/main/java/net/downwithdestruction/dwdcmd/DwDCmd.java
@@ -5,10 +5,6 @@ import com.comphenix.protocol.ProtocolManager;
 import net.downwithdestruction.dwdcmd.commands.Fly;
 import net.downwithdestruction.dwdcmd.commands.Hat;
 import net.downwithdestruction.dwdcmd.listeners.PlayerListener;
-import net.downwithdestruction.dwdcmd.listeners.mobhats.batAbility;
-import net.downwithdestruction.dwdcmd.listeners.mobhats.mobHatSelector;
-import net.downwithdestruction.dwdcmd.listeners.mobhats.packets.pigPackets;
-import net.downwithdestruction.dwdcmd.listeners.mobhats.playerListener;
 import org.bukkit.Bukkit;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.PluginManager;
@@ -34,7 +30,6 @@ public class DwDCmd extends JavaPlugin {
     public static DwDCmd instance;
     private final PluginManager pm = Bukkit.getPluginManager();
     private ProtocolManager protocolManager;
-    private pigPackets pigpackets;
 
     public DwDCmd() {
         instance = this;
@@ -44,16 +39,8 @@ public class DwDCmd extends JavaPlugin {
     public void onEnable() {
         protocolManager = ProtocolLibrary.getProtocolManager();
 
-        pigpackets = new pigPackets(getServer());
-        pigpackets.onMobHatPigSound(protocolManager, this);
-
-
         // Global
         pm.registerEvents(new PlayerListener(this), this);
-        // Mob Hats
-        pm.registerEvents(new playerListener(this), this);
-        pm.registerEvents(new mobHatSelector(this), this);
-        pm.registerEvents(new batAbility(this), this);
 
         registerCommands();
     }
@@ -61,7 +48,6 @@ public class DwDCmd extends JavaPlugin {
     @Override
     public void onDisable() {
         getServer().getScheduler().cancelTasks(this);
-        pigpackets.onMobHatPigSoundRemoval(protocolManager, this);
 
         for (RegisteredListener listener : HandlerList.getRegisteredListeners(this)) {
             try {
@@ -76,6 +62,5 @@ public class DwDCmd extends JavaPlugin {
         this.getCommand("fly").setExecutor(new Fly(this));
         this.getCommand("flyspeed").setExecutor(new Fly(this));
         this.getCommand("helmet").setExecutor(new Hat(this));
-        this.getCommand("mobhat").setExecutor(new Hat(this));
     }
 }
